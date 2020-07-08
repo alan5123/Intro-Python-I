ede438a03e67f7ace4ad0ac1f938babc4d9df60a
alan5123 <alanparker5123@gmail.com>
2020-07-08 03:38:49 -0400

    mvp

--- src/14_cal.py
+++ src/14_cal.py
@@ -2,7 +2,6 @@
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
@@ -17,11 +16,9 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
 Note: the user should provide argument input (in the initial call to run the file) and not 
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
-
 This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
@@ -30,3 +27,25 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+def cal(input=1):
+
+    if(input == 1):
+        x = datetime.now().month
+        print(x)
+    elif(input):
+        array = input.split(" ")
+        print(array)
+        if(len(array) == 2):
+            c = calendar.TextCalendar()
+            str = c.formatmonth(datetime.now().year, int(array[1]))
+            print(str)
+        elif(len(array) == 3):
+            c = calendar.TextCalendar()
+            str = c.formatmonth(int(array[2]), int(array[1]), 0, 0)
+            print(str)
+        elif(len(array) > 3):
+            print('out of bounds')
+
+
+cal('14_cal.py 7 2020 32')
